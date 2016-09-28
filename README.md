1360b93f3db1df1bc9e9d3b51c50b231e813836b
alecsa <alexandra.s709@mail.com>
2016-09-28 18:40:54 +0200

    commit all

---
+++ README.md
@@ -0,0 +1,3 @@
+# A Synchronized Visual and Code Editor for RDF Vocabularies
+
+[Click and try out the demo.](https://rawgit.com/alecsa/MasterThesis/turtle-editor.html)
---
+++ js/config.js
@@ -0,0 +1,18 @@
+var require = {
+  baseUrl: 'js',
+  paths: {
+    mode: '../mode',
+    N3: 'lib/n3-browser-slk',
+    jquery: 'lib/jquery-2.1.3.min',
+    jqueryui: 'lib/jquery-ui',
+    github: 'lib/github',
+    vis: 'lib/vis',
+    underscore: 'lib/underscore-min',
+    waitSeconds: 0
+  }, 
+  shim: { // see http://requirejs.org/docs/api.html#config-shim
+    'N3': {
+      exports: 'N3'
+    }
+  }
+};
